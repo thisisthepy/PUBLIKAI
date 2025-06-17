f6a981d63375634365db7f5d640ba331e55e0aca
BREW <81044362+b-re-w@users.noreply.github.com>
2025-06-17 13:55:05 +0900

    Fix: Reload

--- api/__main__.py
+++ api/__main__.py
@@ -3,12 +3,14 @@ import sys
 
 if sys.argv[1] == "run" and sys.argv[2] == "server":
     try:
-        from api.src.main.server import uvicorn, app
+        from api.src.main.server import uvicorn
+        server_object = "api.src.main.server:app"
     except ImportError:
-        from src.main.server import uvicorn, app
+        from src.main.server import uvicorn
+        server_object = "src.main.server:app"
     host = sys.argv[3] if len(sys.argv) > 3 else "0.0.0.0"
     port = int(sys.argv[4]) if len(sys.argv) > 4 else 23100
     uvicorn.run(
-        app, host=host, port=port, reload=True,
+        server_object, host=host, port=port, reload=True,
         ws_ping_interval=300, ws_ping_timeout=300, ws_per_message_deflate=False
     )
