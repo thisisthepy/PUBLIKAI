c089819a9c8421f213f01fad9160136fe74f06cb
BREW <81044362+b-re-w@users.noreply.github.com>
2025-06-20 13:21:00 +0900

    Fix: Bus ans

--- api/src/main/models/cnuma3/model.py
+++ api/src/main/models/cnuma3/model.py
@@ -119,6 +119,7 @@ Before calling any tools, mentally map out:
 
 5. SHUTTLE BUS (통학/셔틀버스):
     * Note: Please check if today is a public holiday or weekend and decide whether the shuttle bus will be operated or not while referencing the timetable.
+    * Note: Do not provide unnecessary or not prioritized information about the shuttle bus, such as the route or schedule, unless specifically asked.
     - QUESTION A: "지금 탈 수 있는 셔틀버스 있어?"
     - REQUIRED ACTION A: Call `get_shuttle_general_time_table` function and query '셔틀버스 운행 시간표' and COMPARE with CURRENT TIME to find the NEXT AVAILABLE shuttle bus
     - RESPONSE A: "현재 시간 기준으로 다음 캠퍼스 순환 셔틀버스는 {HH}:{MM}분 중앙도서관 출발편입니다. ({minute}분 후) 또한, 다음 교내 순환 셔틀버스는 {HH}:{MM}분에 중앙도서관에서 출발합니다. ({minute}분 후)"
